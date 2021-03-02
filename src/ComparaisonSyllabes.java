c995be7fce5f5a876d2e418ea460a43955a60690
Lysanne Chagnon <gk591048@ens.uqam.ca>
2021-03-01 19:16:49 -0500

    modification de la classe ComparaisonSyllabes

--- src/ComparaisonSyllabes.java
+++ src/ComparaisonSyllabes.java
@@ -1,3 +1,15 @@
 public class ComparaisonSyllabes {
-    
+
+    protected SyllabeFrancais syllabe1;
+
+    protected SyllabeFrancais syllabe2;
+
+    public ComparaisonSyllabes() {
+    }
+
+    public ComparaisonSyllabes(SyllabeFrancais syllabe1,
+                               SyllabeFrancais syllabe2) {
+        this.syllabe1 = syllabe1;
+        this.syllabe2 = syllabe2;
+    }
 }
