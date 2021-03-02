79c2957361fa328cea922c9124f3290002781cc0
Lysanne Chagnon <gk591048@ens.uqam.ca>
2021-03-02 14:36:25 -0500

    Ajout des méthodes de calcule de distances

--- src/ComparaisonSyllabes.java
+++ src/ComparaisonSyllabes.java
@@ -10,12 +10,106 @@ public class ComparaisonSyllabes {
     /******************************************************************************************************************/
     public ComparaisonSyllabes() {
     }
-    /******************************************************************************************************************/
-    // Getters.
-    /******************************************************************************************************************/
+
+    /**
+     * Construit deux syllabes à comparer
+     * @param syllabe1 la première syllabe
+     * @param syllabe2 la deuxième syllabe
+     */
     public ComparaisonSyllabes(SyllabeFrancais syllabe1,
                                SyllabeFrancais syllabe2) {
         this.syllabe1 = syllabe1;
         this.syllabe2 = syllabe2;
+    }
+    /******************************************************************************************************************/
+    // Getters.
+    /******************************************************************************************************************/
+    public SyllabeFrancais getSyllabe1() {
+        return syllabe1;
+    }
+
+    public SyllabeFrancais getSyllabe2() {
+        return syllabe2;
+    }
+
+    /******************************************************************************************************************/
+    // Méthodes de retour.
+    /******************************************************************************************************************/
+
+    /**
+     * à testé (devrait fonctionner vu que ça a fonctionné dans le principal
+     * sans la méthode)
+     * Cette méthode calcule la distance entre les attaques des deux syllabes.
+     * @return la distance entre les attaques
+     */
+    public int distanceAttaque(){
+        int distanceDesAttaques;
+        if(getSyllabe1().getAttaque() == null && getSyllabe2().getAttaque() == null){
+            distanceDesAttaques = 0;
+        }else if (getSyllabe1().getAttaque() == null || getSyllabe2().getAttaque() == null){
+            distanceDesAttaques = 2*6;
+        }else{
+            ComparaisonConsonne attaque1 =
+                    new ComparaisonConsonne(getSyllabe1().getAttaque().getConsonne1(), getSyllabe2().getAttaque().getConsonne1());
+            ComparaisonConsonne attaque2 =
+                    new ComparaisonConsonne(getSyllabe1().getAttaque().getConsonne2(), getSyllabe2().getAttaque().getConsonne2());
+            ComparaisonGroupeConsonne attaque =
+                    new ComparaisonGroupeConsonne(attaque1,attaque2);
+            distanceDesAttaques = attaque.distanceGroupeConsonne();
+        }
+        return distanceDesAttaques;
+    }
+
+    /**
+     * à testé (devrait fonctionner vu que ça a fonctionné dans le principal
+     * sans la méthode)
+     * Cette méthode calcule la distance entre les noyaux des deux syllabes.
+     * @return la distance entre les noyaux
+     */
+    public int distanceNoyau(){
+        ComparaisonVoyelle noyau1 =
+                new ComparaisonVoyelle(getSyllabe1().getNoyau().getSemiVoyelle(), getSyllabe2().getNoyau().getSemiVoyelle());
+        ComparaisonVoyelle noyau2 =
+                new ComparaisonVoyelle(getSyllabe1().getNoyau().getVoyelle(),
+                        getSyllabe2().getNoyau().getVoyelle());
+        ComparaisonGroupeVoyelle noyau = new ComparaisonGroupeVoyelle(noyau1,
+                noyau2);
+        return noyau.distanceGroupeVoyelle();
+    }
+
+    /**
+     * à testé (devrait fonctionner vu que ça a fonctionné dans le principal
+     * sans la méthode)
+     * Cette méthode calcule la distance entre les codas des deux syllabes.
+     * @return la distance entre les codas
+     */
+    public int distanceCoda(){
+        int distanceDesCodas;
+        if(getSyllabe1().getCoda() == null && getSyllabe2().getCoda() == null){
+            distanceDesCodas = 0;
+        }else if (getSyllabe1().getCoda() == null || getSyllabe2().getCoda() == null){
+            distanceDesCodas = 2*6;
+        }else{
+            ComparaisonConsonne coda1 =
+                    new ComparaisonConsonne(getSyllabe1().getCoda().getConsonne1()
+                            , getSyllabe2().getCoda().getConsonne1());
+            ComparaisonConsonne coda2 =
+                    new ComparaisonConsonne(getSyllabe1().getCoda().getConsonne2()
+                            , getSyllabe2().getCoda().getConsonne2());
+            ComparaisonGroupeConsonne coda =
+                    new ComparaisonGroupeConsonne(coda1,coda2);
+            distanceDesCodas = coda.distanceGroupeConsonne();
+        }
+        return distanceDesCodas;
+    }
+
+
+    /**
+     * à testé
+     * Cette méthode calcule la distance entre les deux syllabes.
+     * @return la distance entre les deux syllabes
+     */
+    public int distanceSyllabe(){
+        return distanceAttaque() + distanceNoyau() + distanceCoda();
     }
 }
